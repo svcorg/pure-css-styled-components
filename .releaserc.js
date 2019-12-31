f6714260086139256ef8509a4fc7dbc933ee5772
Chaitanya K <kamthamc@users.noreply.github.com>
2019-12-30 21:53:54 -0800

    fix: ci config

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    ['@semantic-release/npm', { tarballDir: 'dist' }],
     ['@semantic-release/github', { assets: 'dist/*.tgz' }],
   ],
 };
